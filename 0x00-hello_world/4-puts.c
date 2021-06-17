c08c6266e2f8b630817530bad63de6e3c9d9ed50
Andres Ortiz <3283@holbertonschool.com>
2021-06-17 14:12:02 -0700

    First run section 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
 #!/bin/bash
-puts "\"Programming is like building a multilingual puzzle"
+#include <stdio.h> 
+ int main()  
+	  {
+		       puts "\"Programming is like building a multilingual puzzzle"
+				      return(0); 
+				        }
