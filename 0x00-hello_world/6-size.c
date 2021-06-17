513c1256e3d9920d09ee051cb16825e3ce93ac5e
Andres Ortiz <3283@holbertonschool.com>
2021-06-17 16:19:56 -0700

    changed %ld to %zu and corrected Return typo

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  * main - Entry
- * Return: o
+ * Return: 0
  */
 int main(void)
 {
-	printf("Size of a char: %ld bytes\n", sizeof(char));
-	printf("Size of an int: %ld bytes\n", sizeof(int));
-	printf("Size of a long int: %ld bytes\n", sizeof(long int));
-	printf("Size of a long long int: %ld bytes\n", sizeof(long long int));
-	printf("Size of a float: %ld bytes\n", sizeof(float));
-		return (o);
+	printf("Size of a char: %zu bytes\n", sizeof(char));
+	printf("Size of an int: %zu bytes\n", sizeof(int));
+	printf("Size of a long int: %zu bytes\n", sizeof(long int));
+	printf("Size of a long long int: %zu bytes\n", sizeof(long long int));
+	printf("Size of a float: %zu bytes\n", sizeof(float));
+		return (0);
 		}
