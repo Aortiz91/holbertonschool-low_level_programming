5c163d708da3a7de8c38f1d18ddff835f6735ed4
Andres Ortiz <3283@holbertonschool.com>
2021-06-17 14:59:57 -0700

    First run

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* printf - produce output according to a format described
+*
+* Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+			return (0);
+			} 
