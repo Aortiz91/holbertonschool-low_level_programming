fe64a41225966c925f529a6e4c019fe030f1f87a
Andres Ortiz <3283@holbertonschool.com>
2021-06-17 15:55:12 -0700

    First run

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: o
+ */
+int main (void)
+{
+	char charType;
+	int intType;
+	long int longintType;
+	long long longlongType;
+	float floatType;
+
+	printf("Size of char: %ld bytes?n", sizeof(charType);
+	printf("Size of int: %ld bytes?n", sizeof(intType);
+	printf("Size of long int: %ld bytes?n", sizeof(longintType);
+	printf("Size of long long: %ld bytes?n", sizeof(longlongType);
+	printf("Size of float: %ld bytes?n", sizeof(floatType);
+		return (o);
+}
