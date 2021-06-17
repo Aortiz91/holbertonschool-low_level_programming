8b64a1c9c36a02a840cbd7ab420697702c7e5036
Andres Ortiz <3283@holbertonschool.com>
2021-06-17 15:13:19 -0700

    changed the function name in the comment section

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
-* printf - produce output according to a format described
-*
+* main - produce output according to a format described
 * Return: Always 0 (Sucess)
 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\");
-	return (0);
+	return i(0);
 }
