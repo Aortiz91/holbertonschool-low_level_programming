9e9c0baebfa82736875cba78a03cbe13697888b6
Andres Ortiz <3283@holbertonschool.com>
2021-06-17 16:10:12 -0700

    removed designation of variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,10 @@
  */
 int main(void)
 {
-	char charType;
-	int intType;
-	long int longintType;
-	long long longlongType;
-	float floatType;
-
-	printf("Size of char: %ld bytes\n", sizeof(charType));
-	printf("Size of int: %ld bytes\n", sizeof(intType));
-	printf("Size of long int: %ld bytes\n", sizeof(longintType));
-	printf("Size of long long: %ld bytes\n", sizeof(longlongType));
-	printf("Size of float: %ld bytes\n", sizeof(floatType));
+	printf("Size of a char: %ld bytes\n", sizeof(char));
+	printf("Size of an int: %ld bytes\n", sizeof(int));
+	printf("Size of a long int: %ld bytes\n", sizeof(long int));
+	printf("Size of a long long int: %ld bytes\n", sizeof(long long int));
+	printf("Size of a float: %ld bytes\n", sizeof(float));
 		return (o);
 		}
