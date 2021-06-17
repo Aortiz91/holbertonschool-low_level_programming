f04bb33c0e8786e81fe32014ed174d74aefdcef0
Andres Ortiz <3283@holbertonschool.com>
2021-06-17 14:43:18 -0700

    Added Betty format

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
+/**
+ * main - description
+ *
+ * Return: zero
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
