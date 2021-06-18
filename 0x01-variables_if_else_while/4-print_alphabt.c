85a5c0155129b1c3ada5060ceacea3aa2026cb82
Andres Ortiz <3283@holbertonschool.com>
2021-06-18 13:16:37 -0700

    Update github

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry data
+ * Print the alphaber in lowercase followed of a new line
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+int abc;
+for (abc = 'a'; abc <= 'z';)
+if (abc !='e' ; abc !='q')
+putchar (abc);
+putchar ('\n');
+return (0);
+}
