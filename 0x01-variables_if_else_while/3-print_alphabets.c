d857ac2610fa0004d95e6d4a819d5c9ab779584d
Andres Ortiz <3283@holbertonschool.com>
2021-06-18 12:47:51 -0700

    First Run

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry data
+ * Print the alphaber in lowercase and then uppercase followed of a new line
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+int abc;
+int ABC;
+for (abc = 'a'; abc <= 'z'; abc++)
+for (ABC = 'A'; ABC <= 'Z'; ABC++)
+putchar (abc);
+for (ABC = 'A'; ABC <= 'Z'; ABC++)
+putchar (ABC);
+putchar ('\n');
+return (0);
+}
