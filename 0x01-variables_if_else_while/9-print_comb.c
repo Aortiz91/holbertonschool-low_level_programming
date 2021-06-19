d1267b1bb999701de5273b49b9c269c56ea0e150
Andres Ortiz <3283@holbertonschool.com>
2021-06-18 20:00:14 -0700

    first run

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,8 +8,12 @@ int main(void)
 {
 int comb;
 for (comb = '0' ; comb <= '9' ; comb++)
-putchar (comb);
-putchar (',');
+{putchar (comb);
+if (c!='9')
+{putchar (','):
 putchar (' ');
+}
+}
+putchar ('\n');
 return (0);
 }
