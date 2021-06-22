d1667d5a497bfbaa312d82f5c0da8bf3b48a6149
Andres Ortiz <3283@holbertonschool.com>
2021-06-21 19:57:54 -0700

    first run

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry data
+ * Write a program that prints all possible different combinations
+ * of two two digits
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int a;
+	int b;
+		for (a = '0' ; a <= '9' ; a++)
+		{
+			for (b = '0' ; b <= '9' ; b++)
+			{
+				if (!(a > b || a == b))
+				{
+					putchar(a);
+					putchar(b);
+					if (a != '8' || b != '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					else
+						{
+						putchar ('\n');
+						}
+				}
+			}
+		}
+	return (0);
+}
