7b44a3af1bf7292dbb895314d68d5a105899233e
Andres Ortiz <3283@holbertonschool.com>
2021-06-21 19:34:31 -0700

    first run

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 /**
  * main - Entry data
- * Write a program that prints all possible different combinations of two digits
+ * Write a program that prints all possible different combinations
+ * of two digits, no repeating numbers
  * Return: Always (0) Success
  */
 int main(void)
 {
-int comb;
-for (comb = '0' ; comb <= '99' ; comb++)
-{
-putchar (comb)
-if (comb != '99')
-{
-putchar (',');
-putchar (' ');
-}
-}
-putchar ('\n');
-return (0);
+	int a;
+	int b;
+		for (a = '0' ; a <= '9' ; a++)
+		{
+			for (b = '0' ; b <= '9' ; b++)
+			{
+				if (!(a > b || a == b))
+				{
+					putchar('a');
+					putchar('b');
+					if (a != '8' || b != '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+						else
+						{
+						putchar ('\n');
+						}
+				}
+			}
+		}
+	return (0);
 }
