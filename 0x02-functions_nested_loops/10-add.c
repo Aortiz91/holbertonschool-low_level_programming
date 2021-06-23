5fc25473748565780f33b132d56b672ce6697ad8
Andres Ortiz <3283@holbertonschool.com>
2021-06-22 19:56:07 -0700

    first run

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * add - add two integers
+ * @n: first number to add
+ * @m: second number to add
+ * Return: Result to add
+ */
+
+int add(int n, int m)
+{
+	return (n + m);
+
+}
