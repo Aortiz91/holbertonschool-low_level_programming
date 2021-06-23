f732c9eba2e2c9a3deb4685a191567d3a8d44e42
Andres Ortiz <3283@holbertonschool.com>
2021-06-22 18:01:15 -0700

    first run

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_sign - Return sign of n
+ * @n - integer number
+ * Description: Return if n is >,> or = 0
+ * Return: +1, 0 or -1 according to the sign
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (+1);
+	}
+	else if (n == 0)
+	{
+		_putchar (0);
+		return (0);
+	}
+	else
+	{
+		_putchar (-);
+		return (-1);
+	}
+}
