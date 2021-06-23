566924909eead6206cee7b57e4bc5797b459134f
Andres Ortiz <3283@holbertonschool.com>
2021-06-22 18:06:03 -0700

    corrected description and else

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * print_sign - Return sign of n
- * @n - integer number
+ * @n: - integer number
  * Description: Return if n is >,> or = 0
  * Return: +1, 0 or -1 according to the sign
  */
@@ -12,14 +12,14 @@ int print_sign(int n)
 		_putchar ('+');
 		return (+1);
 	}
-	else if (n == 0)
-	{
-		_putchar (0);
-		return (0);
-	}
-	else
+	else if (n < 0)
 	{
 		_putchar ('-');
 		return (-1);
+	}
+	else
+	{
+		_putchar ('0');
+		return (0);
 	}
 }
