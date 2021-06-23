3ed6d5ff894b19939576b1c718fc936393653bd5
Andres Ortiz <3283@holbertonschool.com>
2021-06-23 15:43:10 -0700

    first run

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+/**
+ * print_times_table - Entry point
+ * @n: integer
+ * Description: Print n times table
+ * Return:
+ */
+void print_times_table(int n)
+{
+int a, b, m;
+	if (n <= 15 || n >= 0)
+	{
+	for (a = 0; a <= n ; a++)
+	{
+		for (b = 0; b <= n; b++)
+		{
+			m = (a * b);
+				if (b == 0)
+				{
+					_putchar (m + '0');
+				}
+				else if (m > 99)
+				{
+					_putchar (',');
+					_putchar (' ');
+					_putchar ((m / 100) + '0');
+					_putchar ((m / 10) + '0');
+					_putchar ((m % 10) + '0');
+				}
+				else if (m > 9 && m < 100)
+				{
+					_putchar (',');
+					_putchar (' ');
+					_putchar (' ');
+					_putchar ((m / 10) + '0');
+					_putchar ((m % 10) + '0');
+				}
+				else if (m <= 9 && b != 0)
+				{
+					_putchar (',');
+					_putchar (' ');
+					_putchar (' ');
+					_putchar (' ');
+					_putchar (m + '0');
+				}
+		}
+	_putchar ('\n');
+	}
+	}
+}
