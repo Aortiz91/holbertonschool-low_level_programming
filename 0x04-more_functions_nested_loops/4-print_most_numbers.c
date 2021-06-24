7d2a557699ba3721d9c4c4f7c5f4ba3ca8eac832
Andres Ortiz <3283@holbertonschool.com>
2021-06-24 10:32:12 -0700

    first run

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - Entry point
+ * Description: Function that prints the numbers from 0-9
+ * except 2 and 4
+ * Return: (void)
+ */
+void print_most_numbers(void)
+{
+int c;
+c = 0;
+while ((c < 10) && (c != 2) && (c != 4))
+{
+_putchar(c + '0');
+c++;
+}
+_putchar('\n');
+}
