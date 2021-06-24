7660481e72ab2b5ef85f2c71c2bba88053720460
Andres Ortiz <3283@holbertonschool.com>
2021-06-23 17:34:05 -0700

    run

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,19 @@
 #include "holberton.h"
 /**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
+}
+/**
  * main - check the code for Debigging task 0
  *
  * Return: Always 0.
