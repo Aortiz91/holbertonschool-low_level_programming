9044cbd8500df27f550db003d6ff5e54cd8a09f1
Andres Ortiz <3283@holbertonschool.com>
2021-06-23 17:47:04 -0700

    corrected double function

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -9,18 +9,8 @@ int main(void)
 {
         int i;
 
-        i = 98;
+        i = 0;
         positive_or_negative(i);
 
         return (0);
-}
-/**
- * main - check the code for Debigging task 0
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	positive_or_negative(0);
-	return (0);
 }
