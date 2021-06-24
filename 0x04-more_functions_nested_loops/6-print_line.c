8be4d0f4c91518dc58eb0ff5772b4750584bb291
Andres Ortiz <3283@holbertonschool.com>
2021-06-24 11:19:09 -0700

    first run

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_line - Entry point
+ * @n: number of lines repeats.
+ * Description: Draws a straight line in the termina.
+ * Return:
+ */
+void print_line(int n)
+{
+int a;
+for (a = 0; a <= n; a++)
+{
+_putchar('_');
+}
+if (n <= 0)
+_putchar('\n');
+}
