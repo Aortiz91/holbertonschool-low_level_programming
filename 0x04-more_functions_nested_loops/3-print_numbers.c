8bb18449a1e7155060942f33c6c126b870eba831
Andres Ortiz <3283@holbertonschool.com>
2021-06-24 10:12:56 -0700

    added {}

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,7 +7,11 @@
 void print_numbers(void)
 {
 int a;
-for (a = 0; a <= 9; a++)
-	_putchar(a + '0');
-_putchar('\n');
+	{
+		for (a = 0; a <= 9; a++)
+		{
+			_putchar(a + '0');
+		}
+	_putchar('\n');
+	}
 }
