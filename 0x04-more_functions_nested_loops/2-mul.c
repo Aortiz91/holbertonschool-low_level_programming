8e344918a86b39a13d922372f9c70b52b94b9afb
Andres Ortiz <3283@holbertonschool.com>
2021-06-24 09:18:21 -0700

    furst run

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * mul - Entry point
+ * Description: Multiply two integers
+ * @a: integer
+ * @b: integer
+ * Return: Always (o)
+ */
+int mul(int a, int b)
+{
+int c = (a * b);
+return (0);
+}
