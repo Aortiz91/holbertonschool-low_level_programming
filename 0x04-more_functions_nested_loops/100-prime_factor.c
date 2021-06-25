4bf6a3323ead71f45e2418cecaffd5b5c27a94b3
Andres Ortiz <3283@holbertonschool.com>
2021-06-24 19:44:02 -0700

    corrected betty

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,10 +8,10 @@ int main(void)
 {
 long a;
 long b = 612852475143;
-	for ( a = 2; a < b; a++)
+	for (a = 2; a < b; a++)
 	{
 		if (b % a == 0)
-		b = (b /a);
+		b = (b / a);
 	}
 	printf("%li\n", b);
 	return (0);
