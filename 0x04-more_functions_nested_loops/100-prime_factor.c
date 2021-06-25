3811e435e510716aafe2684f63d0e910cfb239e3
Andres Ortiz <3283@holbertonschool.com>
2021-06-24 19:46:37 -0700

    corrected description

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -3,6 +3,7 @@
  * main - Entry Point
  * Description: Program that finds and prints the largest
  * prime factor of a number
+ * Return: (0)
  */
 int main(void)
 {
@@ -11,7 +12,7 @@ long b = 612852475143;
 	for (a = 2; a < b; a++)
 	{
 		if (b % a == 0)
-		b /= a;
+		b = b / a;
 	}
 	printf("%li\n", b);
 	return (0);
