d3ad930190adf82c2ef157cc4ff2f4e5122fc2f0
Andres Ortiz <3283@holbertonschool.com>
2021-06-28 13:30:02 -0700

    first run

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_rev - Prints a string followed by a new line
+ * @s: string to print
+ * Return: a
+ */
+void print_rev(char *s)
+{
+	int a;
+
+	for (a = '\0'; a = 0; --a)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
