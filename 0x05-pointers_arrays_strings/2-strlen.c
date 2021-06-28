b8634ab50c01464e972138151b94ff4a1fa75570
Andres Ortiz <3283@holbertonschool.com>
2021-06-28 12:20:34 -0700

    removed putchar

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,12 @@
 #include "holberton.h"
 /**
  * _strlen - Entry point.
- * @s: character 
+ * @s: string 
  * Write a function that returns the length of a string.
  */
 int _strlen(char *s)
 {
 	int a;
 	for (a = 0; s[a] != '\0'; a++)
-		_putchar(a);
 	return (a);
 }
