6c9afed02e7f2748e57d2b97f2836a8ab23c962f
Andres Ortiz <3283@holbertonschool.com>
2021-06-28 12:54:31 -0700

    corrected style

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,14 @@
 #include "holberton.h"
 /**
  * _strlen - Entry point.
- * @s: string 
- * Write a function that returns the length of a string.
+ * @s: string
+ * Description: Write a function that returns the length of a string.
  */
 int _strlen(char *s)
 {
 	int a;
 	a = 0;
+
 	while (s[a] != '\0')
 {
 	a++;
