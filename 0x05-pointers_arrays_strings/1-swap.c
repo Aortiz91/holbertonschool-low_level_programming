05c842df70497c6f2f1c91a559d110aba190ea65
Andres Ortiz <3283@holbertonschool.com>
2021-06-28 11:40:08 -0700

    first run

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * swap_int - Entry point
+ * @a: integer a 
+ * @b: integer b
+ * Description:Write a function that swaps the values of two integers.
+ */
+void swap_int(int *a, int *b)
+{
+int c;
+c = *a:
+*a = *b;
+*b = c;
+}
