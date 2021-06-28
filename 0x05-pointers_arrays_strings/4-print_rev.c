411c24fddede23a5940a463289a9fc5079104ce8
Andres Ortiz <3283@holbertonschool.com>
2021-06-28 15:07:09 -0700

    removed nested function

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,20 @@
 #include "holberton.h"
 /**
  * print_rev - Prints a string followed by a new line
- * _strlen - prints the lengh of a string
  * @s: string to print
  */
 void print_rev(char *s)
 {
-int len, a;
-	int _strlen(char *s)
+	int len, a;
+
+	while (s[len] != '\0')
 	{
-		while (s[len] != '\0')
-		{
 		len++;
-		}
+	}
 	len = len;
 	for (a = len; a >= 0; a--)
 	{
 		_putchar(s[a]);
 	}
 	_putchar('\n');
-	}
 }
