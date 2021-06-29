8309438ba01da37f150fddf30636197d6c548fa8
Andres Ortiz <3283@holbertonschool.com>
2021-06-28 19:47:43 -0700

    run

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * *_strcpy - Copies string pointed to by src to
+ * buffer pointed to by dest
+ * @dest: pointer destination
+ * @str: pointer source
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+	a = 0;
+	
+	while (src[a])
+	{
+		dest[a] = src [a];
+		a++;
+	}
+	return(dest);
+}
