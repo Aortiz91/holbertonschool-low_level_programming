a39151cc78b59cad053faaf9f9545cc45073ed22
Andres Ortiz <3283@holbertonschool.com>
2021-06-28 19:12:26 -0700

    run

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,19 @@
+#include "holberton.h"
+/**
+ * puts2 - Function that prints every other character.
+ * @str: string
+ */
+void puts2(char *str)
+{
+	int l;
 
+	l = 0;
+
+	while (str[l] != '\0')
+	{
+		if (l % 2 == 0)
+			_putchar(str[l]);
+	l++;
+	}
+	_putchar('\n');
+}
