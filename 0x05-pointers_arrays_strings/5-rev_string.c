9f1320016fd85ec6156342c1d76611cc145c5aa3
Andres Ortiz <3283@holbertonschool.com>
2021-06-28 17:44:06 -0700

    run

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,25 @@
 #include "holberton.h"
 /**
- *rev_string - Reverse string 
- *@s: char 
+ * rev_string - Reverse a string
+ * @s: string
  */
-void rev_string(char *s)
+void print_rev(char *s)
+{
+	int a, b, c, com;
 
+	a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+
+	b = a - 1;
+
+	for (c = 0; c < (b / 2); c++)
+	{
+		com = s[c];
+		s[c] = s[b];
+		s[b--] = com;
+	}
+}
