e75e1174acba054dd7ad122b94cda8a12c8af64d
Andres Ortiz <3283@holbertonschool.com>
2021-07-01 19:53:59 -0700

    run

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * *cap_string - Function that capitalizes all words of a string.
+ * @str: string
+ * Return: Always 0
+ */
+char *cap_string(char *str)
+{
+	int a, b;
+	char sep[13] = {' ', '\t', '\n', ',', ';', '.', '!',
+		'?', '"', '(', ')', '{', '}'};
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		if (a == 0 && str[a] >= 97 && str[a] <= 122)
+			str[a] -= 32;
+		for (b = 0; sep[b] <= 13; b++)
+			if (str[a] == sep[b])
+				str[a + 1] -= 32;
+	}
+	return (str);
+}
