5cbb72ae8ca096f0e43d844ad2e936942b355120
Andres Ortiz <3283@holbertonschool.com>
2021-07-06 09:53:20 -0700

    commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,13 +9,15 @@ char *_strstr(char *haystack, char *needle)
 {
 	int a, b;
 
-	for (b = 0; needle[b] != '\0'; b++)
+	for (a = 0; haystack[a] != '\0'; a++)
 	{
-		for (a = 0; haystack[a] != '\0'; a++)
+		for (b = 0; needle[b] != '\0'; b++)
 		{
-			if (haystack[a] == needle[b])
-				return (haystack + a);
+			if (haystack[a + b] != needle[b])
+				break;
 		}
+		if (!needle[b])
+			return(&haystack[a]);
 	}
 	return ('\0');
 }
