6e9a678ed4cdb2df458bb4cfc8fe6da0d7378f19
Andres Ortiz <3283@holbertonschool.com>
2021-07-07 13:34:52 -0700

    Recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - Write a function that returns the value
+ * of x raised to the power of y.
+ * @x: integer X
+ * @y_ integer Y
+ * Return: Value of x ^ y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
