e61a0f049e5df6f7120ed064177d89b7277a584a
Andres Ortiz <3283@holbertonschool.com>
2021-07-07 13:35:38 -0700

    Recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
  * _pow_recursion - Write a function that returns the value
  * of x raised to the power of y.
  * @x: integer X
- * @y_ integer Y
+ * @y: integer Y
  * Return: Value of x ^ y
  */
 int _pow_recursion(int x, int y)
