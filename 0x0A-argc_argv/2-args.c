3afb20c1b047753d8ef7f80743eb7e77971138e5
Andres Ortiz <3283@holbertonschool.com>
2021-07-09 11:45:45 -0700

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints all arguments it receives.
+ * @argc: count of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+		printf("%s\n", argv[a]);
+	return (0);
+}
