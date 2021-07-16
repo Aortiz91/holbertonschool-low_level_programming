84da36dea7d026a558fb11efc0d2bc4a4dc11bcf
Andres Ortiz <3283@holbertonschool.com>
2021-07-15 19:42:13 -0700

    malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,23 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
+ * *_memset -  Write a function that fills memory with a constant byte.
+ * @s: memory
+ * @b: char to copy
+ * @n: times to copy b
+ * Return: (s) pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b;
+	}
+	return (s);
+}
+/**
  * _calloc -  Write a function that allocates memory for an array,
  * using malloc.
  * @nmemb: number of elements
@@ -10,14 +27,12 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *p;
-	unsigned int a = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(size * nmemb);
 	if (!p)
 		return (NULL);
-	for (a = 0; a < nmemb; a++)
-		p[a] = 0;
+	_memset(p, 0, nmemb * size);
 	return (p);
 }
