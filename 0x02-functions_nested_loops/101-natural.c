67426036a9fbc083c27fc55b70f6304ddde61031
Andres Ortiz <3283@holbertonschool.com>
2021-07-18 19:18:33 -0700

    Review

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	long unsigned int a, sum;
-	long unsigned int n = 0, m = 0;
+	unsigned long int a, sum;
+	unsigned long int n = 0, m = 0;
 
 	for (a = 1; a < 1024; a++)
 		if (a % 3 == 0)
 			m = m + a;
-		else if (m % 5 == 0)
+		else if (a % 5 == 0)
 			n = n + a;
 	sum = n + m;
 	printf("%lu\n", sum);
