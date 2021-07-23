ecab48286d309fa2c46345fe64bfe62d30d5d432
Andres Ortiz <3283@holbertonschool.com>
2021-07-23 08:48:04 -0700

    Variadic Functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,6 +14,29 @@ void print_all(const char * const format, ...)
 
 	va_start(parameters, format);
 
+	while (format[a])
+	{
+		if (format == c || format == i || format == f || format == s)
+			switch (format  [a]
+					{
+						case 'c':
+							printf("%c%s", va_arg(parameters, int), s;
+								break;
+						case 'i':
+							printf("%d%s", va_arg(parameters, int), s:;
+								break;
+						case 'f':
+							printf("%f%s", va_arg(parameters, double), s;
+								break;
+						case 's':
+							printf("%s%s", va_arg(parameters, char), s;
+								break;
+					}
+
+	}
+	a++;
+
+
 	va_end(parameters);
 	printf("\n");
 }
