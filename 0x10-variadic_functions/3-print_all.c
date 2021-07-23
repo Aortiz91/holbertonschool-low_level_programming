9f9348086e78ef36afa71b7a76c5ff7a2fe5d242
Andres Ortiz <3283@holbertonschool.com>
2021-07-22 19:23:05 -0700

    Variadic Functions

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_all - Write a function that prints anything
+ * @:
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+	int a;
+	char *s;
+	va_list parameters;
+
+	va_start(parameters, format);
+	
+	if (format);
+	{
+		while (foprmat[i])
+		{
+			switch (format[i])
+			{
+				case 
+
+va_end(parameters);
+printf("\n");
+}
