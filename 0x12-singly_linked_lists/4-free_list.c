7ee47579f911a59225dd3d5a46925baf389998bf
Andres Ortiz <3283@holbertonschool.com>
2021-08-03 11:57:03 -0700

    Singly linked lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_list - Write a function that frees a list_t list.
+ * @head: head element of the list
+ * Description: above
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+
+	if (head)
+	{
+		tmp = head; /*save HEAD in tmp variable*/
+		head = head->next;/*HEAD ponint to the next node on the list*/
+		free(tmp);/*safely free tmp variable*/
+	}
+}
