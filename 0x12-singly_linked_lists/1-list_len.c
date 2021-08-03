879288fe405b5d070dd2ed9526694aee2a3c14ba
Andres Ortiz <3283@holbertonschool.com>
2021-08-03 06:05:38 -0700

    Singly linked lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
- *list_len - Write a function that returns the number of elements in a
- *linked list_t list.
- *@h: elements of the list
- *Description: above
- *Return:
+ * list_len - Write a function that returns the number of elements in a
+ * linked list_t list.
+ * @h: elements of the list
+ * Description: above
+ * Return: number of elements
  */
 size_t list_len(const list_t *h)
 {
@@ -12,16 +12,8 @@ size_t list_len(const list_t *h)
 
 		while (h)
 		{
-			if (h->str == NULL)
-			{
-				printf("[0] (nil)\n");
-			}
-			else
-			{
-				printf("[%d] %s\n", h->len, h->str);
-			}
-		nodes++;
-		h = h->next;
+			nodes++;
+			h = h->next;
 		}
-	return (nodes);
+		return (nodes);
 }
