8ecffd8821cf4fba76c564d725fd6dd9b303854f
Andres Ortiz <3283@holbertonschool.com>
2021-08-04 10:03:42 -0700

    More singly linked lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * listint_len - Write a function that returns the number of elements in a 
+ * linked listint_t list
+ * @h: elements of my struct
+ * Return: Number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+/*		printf("%d\n", h->n);*/
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
