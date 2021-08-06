3665945b89a9f378433c2f48371a25cb4b6e732a
Andres Ortiz <3283@holbertonschool.com>
2021-08-05 19:59:58 -0700

    Bit Manipulation

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * set_bit - Write a function that returns the value of a bit at a given index
+ * @n: entire number
+ * @index: bit to look for
+ * Return: Value of the bit at a index index
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int value;
+
+	if (index > 64)
+		return (-1);
+	for (value = 1; index > 0; index--, value *= 2);
+	*n += value;
+		return (1);
+}
