9d0744a888f98f128f902e99ec1acd02fbeb3d58
Andres Ortiz <3283@holbertonschool.com>
2021-08-06 10:21:29 -0700

    Bit Manipulation

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,7 +10,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 64) /*I will only have 64 bis*/
 		return (-1);
-	*n = *n & (1 << index); /*bitwise left shift "index" times*/
+	*n = *n & (1 >> index); /*bitwise rigth shift "index" times*/
 				 /*and used operator AND to convert*/
 				 /* my byte to 0*/
 	return (1);
