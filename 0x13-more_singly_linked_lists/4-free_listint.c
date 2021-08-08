8e9828e852a7b9b42a841fcf11bf8b98a0996bf1
Andres Ortiz <3283@holbertonschool.com>
2021-08-08 09:12:30 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 /**
  * free_listint - Write a function that frees a listint_t list.
- * @head: 
+ * @head: elements of the list.
  */
 void free_listint(listint_t *head)
 {
-	listint_t* tmp;
-	
+	listint_t  *tmp;
+
 	while (head != NULL) /*If head is NULL the list is already empty*/
 	{
 		tmp = head;
