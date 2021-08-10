21d809a5d0c02a9224c1f2d1ace71bda659b52c0
Andres Ortiz <3283@holbertonschool.com>
2021-08-10 10:43:59 -0700

    File I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,10 +19,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	else
 		length = 0;
-	o = open(filename, O_RDWR);
+	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, length);
 	if (o == -1 || w == -1)
 		return (-1);
 	close(o);
+	if (length != w)
+		return (-1);
 	return (1);
 }
