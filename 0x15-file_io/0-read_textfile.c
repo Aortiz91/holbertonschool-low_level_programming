6b7951adf4a8fdf2f86c9112faeb49624bc5cd13
Andres Ortiz <3283@holbertonschool.com>
2021-08-09 19:09:05 -0700

    File I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * read_text - Write a function that reads a text file and prints it to the
+ * POSIX standard output. 
+ * @filename: Name of the file
+ * @letters: number of letters the program should read and print
+ * Return: Actual number of letters it could read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
