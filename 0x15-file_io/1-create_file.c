b120f57be4be53e3836810866a35a19e3453ec2f
Andres Ortiz <3283@holbertonschool.com>
2021-08-10 10:00:07 -0700

    File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,9 @@ int create_file(const char *filename, char *text_content)
 		for (length = 0; text_content[length]; length++)
 		;
 	}
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	else
+		length = 0;
+	o = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	w = write(o, text_content, length);
 	if (o == -1 || w == -1)
 		return (-1);
