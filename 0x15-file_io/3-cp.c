4874f1bccc4668003212798d24f392fb7c9900da
Andres Ortiz <3283@holbertonschool.com>
2021-08-13 18:46:21 -0700

    File

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * main - Write a program that copies the content of a file to another file.
+ * @argc: count of arguments 
+ * @argv: pointer to arguments
+ * Return: 0 Success
+ */
+int main(int argc, char *argv[])
+{
+	int o_from, o_to, r, w;
+	char buffer[1024];
+
+	if (argc != 3) /*as I will only have 2 more arguments: from and to*/
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	if (argv[1] == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	}
+	o_from = open(argv[1], O_RDONLY);
+	o_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	r = 1024;
+	while (r == 1024)
+	{	r = read(o_from, buffer, r);
+		w = write(o_to, buffer, r);
+		if (w == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+	close(o_from);
+	close(o_to);
+	if (o_from == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", o_from), exit(100);
+	if (o_to == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", o_to), exit(100);
+	return (0);
+}
