6e4cf43f5a363f528bee643382d36aba9ae81cc9
Andres <3283@holbertonschool.com>
2021-09-16 11:34:39 -0700

    0x17. C - Doubly linked lists

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,15 +7,14 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *tmp, *new_node;
+	dlistint_t *new_node;
 
-	new_node = malloc(sizeof(struct dlistint_t*));
+	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return NULL;
-	tmp = head;
-	new_node->data = n;
-	new_node->next = head;
-	head = new_node;
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
 	
-	return (&head);
+	return (new_node);
 }
