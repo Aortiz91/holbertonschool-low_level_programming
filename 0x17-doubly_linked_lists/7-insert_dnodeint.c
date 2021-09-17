12311d6448232938878194669a2ce771b9313d91
Andres <3283@holbertonschool.com>
2021-09-17 12:19:37 -0700

    0x17. C - Doubly linked lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,9 +11,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *tmp = *h, *new_node;
 	unsigned int i = 0;
 
+	if (idx < 0)
+		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
