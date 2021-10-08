6ba15eb3b55f096467d1e707378143fb1872c5ea
Andres Ortiz <3283@holbertonschool.com>
2021-10-08 07:28:49 -0700

    0x1A. C - Hash tables

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+/**
+ * key_index - Determine the index of a key
+ * @key: Key to be stored
+ * @size: Size of the array of the hash
+ *
+ * Return: Index at which the key/value should be stored
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash, i;
+	hash = 0;
+	i = 0;
+
+	while(key & key[i])
+	{
+		hash = (hash + key[i]) % size;
+		i++
+	}
+	return (hash);
+}
