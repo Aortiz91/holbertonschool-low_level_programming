1900f07cc4deb9edf496f041b72d59ebbc1e5da0
Andres Ortiz <3283@holbertonschool.com>
2021-10-08 06:53:48 -0700

    0x1A. C - Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,5 @@
 #include "hash_tables.h"
 #include <stdlib.h>
-#include <string.h>
 /**
  * hash_table_create - Allocate the requisite memory for a new hash_table_t
  * and it's indexed array. Assign size of the array in new_table->size
@@ -17,13 +16,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table = malloc(sizeof(hash_table_t));
 	if (!new_table)
 		return (NULL);
-	new_table->array = (hash_node_t **)malloc(size * sizeof(hash_node_t));
+	new_table->size = size;
+	new_table->array = calloc(new_table->size, sizeof(hash_node_t));
 	if (!(new_table->array))
 	{
 		free(new_table);
 		return (NULL);
 	}
-	memset(new_table->array, 0, size * sizeof(hash_node_t));
-	new_table->size = size;
 	return (new_table);
 }
