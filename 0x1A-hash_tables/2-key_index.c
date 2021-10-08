c32de74b7b1592f8f5a6fcaa1ed7a4e10d811f56
Andres Ortiz <3283@holbertonschool.com>
2021-10-08 09:53:26 -0700

    0x1A. C - Hash tables

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,10 +10,11 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash, i;
+
 	hash = 0;
 	i = 0;
 
-	while(key && key[i])
+	while (key && key[i])
 	{
 		hash = (hash + key[i]) % size;
 		i++;
