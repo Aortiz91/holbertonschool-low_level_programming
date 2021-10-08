c08e4e4b937485d761b52dcfe6620a52d7c7924b
Andres Ortiz <3283@holbertonschool.com>
2021-10-08 07:04:04 -0700

    0x1A. C - Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table = malloc(sizeof(hash_table_t));
 	if (!new_table)
 		return (NULL);
-	new_table->array = calloc(new_table->size, sizeof(hash_node_t));
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (!(new_table->array))
 	{
 		free(new_table);
